c92273b6976ca31d861399525923d0c27f812d61
Zach Ickes <zachickes@hotmail.com>
2021-09-20 21:03:14 -0600

    finished knight class, still need to build the graph to solve the problem

--- lib/knight.rb
+++ lib/knight.rb
@@ -2,7 +2,7 @@ require_relative 'board.rb'
 
 # class for generating knight objects for a graph
 class Knight
-  attr_reader :moves
+  attr_reader :moves, :parent, :connected_knights, :position
   def initialize(position, parent = nil)
     @position = position
     @parent = parent
@@ -12,6 +12,7 @@ class Knight
     moves
   end
 
+  # adding all the legal moves to the moves array
   def moves
     move_one
     move_two
@@ -23,63 +24,91 @@ class Knight
     move_eight
   end
 
+  # adding all logic to determine which moves are legal
   def move_one
-    move_one = @position
+    move_one = []
+    move_one << @position[0]
+    move_one << @position[1]
     move_one[0] += 1
     move_one[1] += 2
-    p move_one
     @moves << move_one if @board.board.include?(move_one)
   end
 
   def move_two
-    move_two = @position
+    move_two = []
+    move_two << @position[0]
+    move_two << @position[1]
     move_two[0] += 2
     move_two[1] += 1
     @moves << move_two if @board.board.include?(move_two)
   end
 
   def move_three
-    move_three = @position
+    move_three = []
+    move_three << @position[0]
+    move_three << @position[1]
     move_three[0] -= 1
     move_three[1] += 2
     @moves << move_three if @board.board.include?(move_three)
   end
 
   def move_four
-    move_four = @position
+    move_four = []
+    move_four << @position[0]
+    move_four << @position[1]
     move_four[0] += 1
     move_four[1] -= 2
     @moves << move_four if @board.board.include?(move_four)
   end
 
   def move_five
-    move_five = @position
+    move_five = []
+    move_five << @position[0]
+    move_five << @position[1]
     move_five[0] -= 2
     move_five[1] -= 1
     @moves << move_five if @board.board.include?(move_five)
   end
 
   def move_six
-    move_six = @position
+    move_six = []
+    move_six << @position[0]
+    move_six << @position[1]
     move_six[0] -= 1
     move_six[1] -= 2
     @moves << move_six if @board.board.include?(move_six)
   end
 
   def move_seven
-    move_seven = @position
+    move_seven = []
+    move_seven << @position[0]
+    move_seven << @position[1]
     move_seven[0] -= 2
     move_seven[1] += 1
     @moves << move_seven if @board.board.include?(move_seven)
   end
 
   def move_eight
-    move_eight = @position
+    move_eight = []
+    move_eight << @position[0]
+    move_eight << @position[1]
     move_eight[0] += 2
     move_eight[1] -= 1
     @moves << move_eight if @board.board.include?(move_eight)
   end
+
+  # Uses legal moves to create new knight objects
+  def make_knights
+    @moves.each do |move|
+      if parent == nil
+        knight = Knight.new(move, self)
+        @connected_knights << knight
+      else
+        knight = nil
+        knight = Knight.new(move, self) unless move == @parent.position
+        @connected_knights << knight unless knight == nil
+      end
+    end
+  end
 end
 
-knight = Knight.new([3,3])
-p knight.moves
